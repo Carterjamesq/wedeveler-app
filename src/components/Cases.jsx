067b040e7d8ced3d07c670b4906a5a51d5e2813e
Vitalii Rabchebskyi <carterjames@ukr.net>
2024-01-11 21:42:07 +0200

    Deleting Lightbox

--- src/components/Cases.jsx
+++ src/components/Cases.jsx
@@ -1,84 +1,67 @@
-import React, { useEffect, useState, useRef } from 'react';
-import ScrollReveal from 'scrollreveal';
-import { createClient } from 'contentful';
-import { MdArrowOutward } from 'react-icons/md';
-import Lightbox from 'react-image-lightbox';
-import 'react-image-lightbox/style.css';
-
+import React, { useEffect, useState, useRef } from "react";
+import ScrollReveal from "scrollreveal";
+import { createClient } from "contentful";
+import { MdArrowOutward } from "react-icons/md";
 import { IoIosBriefcase } from "react-icons/io";
 
 const Cases = () => {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [casePosts, setCasePosts] = useState([]);
+  const [visiblePosts, setVisiblePosts] = useState(2);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
+  const client = createClient({
+    space: "umsxju4f68ej",
+    accessToken: "-1blGBV05WJ2AA5h7ViDEmxEE00dNxOZOtYCZ0wFmiI",
+  });
 
-    const [casePosts, setCasePosts] = useState([]);
-    const [visiblePosts, setVisiblePosts] = useState(2);
-    const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
-    const [photoIndex, setPhotoIndex] = useState(0);
-    const client = createClient({
-        space: 'umsxju4f68ej',
-        accessToken: '-1blGBV05WJ2AA5h7ViDEmxEE00dNxOZOtYCZ0wFmiI',
-    });
-
-    useEffect(() => {
-        const getAllCaseEntries = async () => {
-        try {
-            const entries = await client.getEntries({ content_type: 'portfolio' });
-            setCasePosts(entries.items);
-        } catch (error) {
-            console.error(error);
-        }
-        };
-        getAllCaseEntries();
-    }, []);
-
-    const loadMore = () => {
-        setVisiblePosts((prevVisiblePosts) => prevVisiblePosts + 2);
+  useEffect(() => {
+    const getAllCaseEntries = async () => {
+      try {
+        const entries = await client.getEntries({ content_type: "portfolio" });
+        setCasePosts(entries.items);
+      } catch (error) {
+        console.error(error);
+      }
     };
+    getAllCaseEntries();
+  }, []);
 
-    const boxRef = useRef();
+  const loadMore = () => {
+    setVisiblePosts((prevVisiblePosts) => prevVisiblePosts + 2);
+  };
 
-    useEffect(() => {
-        const config = {
-        origin: 'bottom',
-        duration: 850,
-        delay: 150,
-        distance: '500px',
-        scale: 1,
-        easing: 'ease',
-        };
-        ScrollReveal().reveal(boxRef.current, config);
-    }, []);
+  const boxRef = useRef();
 
-    const openLightbox = (index) => {
-        setLoading(true);
-        setLightboxIsOpen(true);
-        setPhotoIndex(index);
-      };
-    
-      const closeLightbox = () => {
-        setLightboxIsOpen(false);
-        setPhotoIndex(0);
-      };
-    
-      const handleResize = () => {
-        setIsMobile(window.innerWidth < 992);
-      };
-    
-      useEffect(() => {
-        window.addEventListener('resize', handleResize);
-    
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-      }, []);
-    
-      useEffect(() => {
-        if (lightboxIsOpen && casePosts[photoIndex]?.fields.projectImage?.fields.file.url) {
-          setLoading(false);
-        }
-      }, [lightboxIsOpen, photoIndex, casePosts]);
-        
+  useEffect(() => {
+    const config = {
+      origin: "bottom",
+      duration: 850,
+      delay: 150,
+      distance: "500px",
+      scale: 1,
+      easing: "ease",
+    };
+    ScrollReveal().reveal(boxRef.current, config);
+  }, []);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 992);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (loading) {
+      setLoading(false);
+    }
+  }, [loading]);
+
   return (
     <div className="section-padding" ref={boxRef}>
       <div className="col d-flex icon">
@@ -92,22 +75,23 @@ const Cases = () => {
         {casePosts.slice(0, visiblePosts).map((portfolio, index) => (
           <div className="card mb-4" key={portfolio.sys.id}>
             <div className="image-container">
-                {portfolio.fields.projectImage &&
+              {portfolio.fields.projectImage &&
                 portfolio.fields.projectImage.fields &&
                 portfolio.fields.projectImage.fields.file && (
-                    <img
+                  <img
                     className="rounded-3"
                     src={portfolio.fields.projectImage.fields.file.url}
                     alt={portfolio.fields.projectTitle}
-                    onClick={() => openLightbox(index)}
-                    />
+                  />
                 )}
-              <div className='tags'>
-                  {portfolio.fields.tags && portfolio.fields.tags.map && (
-                      portfolio.fields.tags.map((tag, index) => (
-                          <span key={index} className="badge btn-primary me-2">{tag}</span>
-                      ))
-                  )}
+              <div className="tags">
+                {portfolio.fields.tags &&
+                  portfolio.fields.tags.map &&
+                  portfolio.fields.tags.map((tag, index) => (
+                    <span key={index} className="badge btn-primary me-2">
+                      {tag}
+                    </span>
+                  ))}
               </div>
             </div>
             <div className="row">
@@ -118,6 +102,7 @@ const Cases = () => {
                     className="btn btn-primary"
                     href={portfolio.fields.projectLink}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     Visit <MdArrowOutward />
                   </a>
@@ -133,26 +118,6 @@ const Cases = () => {
             Load More
           </button>
         </div>
-      )}
-      {!isMobile && lightboxIsOpen && (
-        <Lightbox
-          mainSrc={
-            loading
-              ? '/path/to/loading-spinner.gif'
-              : casePosts[photoIndex]?.fields.projectImage.fields.file.url || '/path/to/fallback-image.jpg'
-          }
-          nextSrc={
-            casePosts[(photoIndex + 1) % casePosts.length]?.fields.projectImage.fields.file.url
-          }
-          prevSrc={
-            casePosts[(photoIndex + casePosts.length - 1) % casePosts.length]?.fields.projectImage.fields.file.url
-          }
-          onCloseRequest={closeLightbox}
-          onMovePrevRequest={() =>
-            setPhotoIndex((photoIndex + casePosts.length - 1) % casePosts.length)
-          }
-          onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % casePosts.length)}
-        />
       )}
     </div>
   );
