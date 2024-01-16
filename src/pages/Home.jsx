3b439f6f020e4a80a61adfdf6f08929ccd8b33f7
Vitalii Rabchebskyi <carterjames@ukr.net>
2024-01-16 12:06:17 +0200

    Deleting <pre> tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,6 @@ const Home = () => {
           <ProfileCard />
         </div>
         <div className="col-lg">
-          <pre>Some Text</pre>
           <HomeBanner />
           <Counts />
           <Expirience />
