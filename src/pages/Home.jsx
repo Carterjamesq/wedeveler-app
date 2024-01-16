04979e1e881af195de29b8805fc56b33c3f05890
Vitalii Rabchebskyi <carterjames@ukr.net>
2024-01-16 11:45:31 +0200

    Changese

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ const Home = () => {
           <ProfileCard />
         </div>
         <div className="col-lg">
+          <pre>Text</pre>
           <HomeBanner />
           <Counts />
           <Expirience />
