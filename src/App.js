96c7521d82379720fed503ae29fef3509aa658b0
Vitalii Rabchebskyi <carterjames@ukr.net>
2024-07-06 11:07:14 +0300

    Adding Expirience

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import Home from './pages/Home'
-import ThemeProvider from './ThemeProvider';
-import Switch from './Switch';
-
-
+import Home from "./pages/Home";
+import ThemeProvider from "./ThemeProvider";
+import Switch from "./Switch";
 
 function App() {
   return (
-      <div className="App">
-        <ThemeProvider>
-          {({ darkMode, toggleDarkMode }) => (
-            <div style={{ background: darkMode ? '#0d0d0d' : '#fff', color: darkMode ? '#fff' : '#0d0d0d' }}>
-              <Switch checked={darkMode} onChange={toggleDarkMode} />
-              <Home/>
-            </div>
-          )}
-        </ThemeProvider>
-        
-      </div>
+    <div className="App">
+      <ThemeProvider>
+        {({ darkMode, toggleDarkMode }) => (
+          <div
+            style={{
+              background: darkMode ? "#0d0d0d" : "#fff",
+              color: darkMode ? "#fff" : "#0d0d0d",
+            }}
+          >
+            <Switch checked={darkMode} onChange={toggleDarkMode} />
+            <Home />
+          </div>
+        )}
+      </ThemeProvider>
+    </div>
   );
 }
 
