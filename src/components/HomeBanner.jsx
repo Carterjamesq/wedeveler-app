0b899dfd1809d429da79bb7d89661734bca187f4
Vitalii Rabchebskyi <carterjames@ukr.net>
2024-11-25 15:00:07 +0200

    Updated Counts Hero

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useRef } from 'react';
-import ReactTypingEffect from 'react-typing-effect';
+import React, { useEffect, useRef } from "react";
+import ReactTypingEffect from "react-typing-effect";
 
-import ScrollReveal from 'scrollreveal';
-
+import ScrollReveal from "scrollreveal";
 import { IoMdHome } from "react-icons/io";
 
-
-
 const HomeBanner = () => {
-
   const boxRef = useRef();
 
   useEffect(() => {
     const config = {
-      origin: 'bottom',
+      origin: "bottom",
       duration: 700,
       delay: 150,
-      distance: '500px',
+      distance: "500px",
       scale: 1,
-      easing: 'ease',
+      easing: "ease",
     };
     ScrollReveal().reveal(boxRef.current, config);
   }, []);
 
-
   return (
     <div ref={boxRef}>
       <div className="col d-flex icon">
-        <div className='border rounded-5 px-4 py-2 mb-5'>
+        <div className="border rounded-5 px-4 py-2 mb-5">
           <IoMdHome />
           <p>Introducing</p>
         </div>
       </div>
-      <div className='h1 mb-5 display-1'>Hi, I'm <span>Vitalyi</span>,<br></br> Front-end Developer &nbsp;<br></br>
+      <div className="h1 mb-5 display-1">
+        Hi, I'm <span>Vitalyi</span>,<br></br> Front-end Developer &nbsp;
+        <br></br>
         <ReactTypingEffect
-          text={["Wordpress", "Headless", "<React />"]}
+          text={["WordPress", "Webflow", "Headless", "<React />"]}
           speed={150}
           typingDelay={300}
           eraseDelay={600}
         />
       </div>
-      <p className='mb-5 pb-5 grey-text'>With a knack for translating design into seamless, responsive experiences,<br></br>
-        I'm here to enhance your online presence. Let's collaborate to bring your digital vision to life!
+      <p className="mb-5 pb-5 grey-text">
+        With a knack for translating design into seamless, responsive
+        experiences,<br></br>
+        I'm here to enhance your online presence. Let's collaborate to bring
+        your digital vision to life!
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
